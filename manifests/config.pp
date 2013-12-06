f6d5b9165f7b8c18748523c55bc68fa5add1460e
Samuli Seppänen <samuli.seppanen@gmail.com>
2013-12-06 20:36:12 +0200

    Started fetching some variables from os::params
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,12 +10,15 @@ class sshd::config(
     $passwordauthentication
 )
 {
+
+    include os::params
+
     file { 'sshd-sshd_config':
         name    => '/etc/ssh/sshd_config',
         content => template('sshd/sshd_config.erb'),
         ensure  => present,
         owner   => root,
-        group   => "${::sshd::params::admingroup}",
+        group   => "${::os::params::admingroup}",
         mode    => 644,
         require => Class['sshd::install'],
         notify  => Class['sshd::service'],
