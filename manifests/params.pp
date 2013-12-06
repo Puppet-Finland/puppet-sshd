e4c7156e919095df098f9b49ef4756ccb8bccc92
Samuli Seppänen <samuli.seppanen@gmail.com>
2013-12-06 20:40:52 +0200

    Made monit start/stop rules OS-agnostic
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,21 +9,33 @@ class sshd::params {
         'RedHat': {
             $package_name = 'openssh-server'
             $service_name = 'sshd'
-            $service_command = "/sbin/service $service_name"
+
+            if $::operatingsystem == 'Fedora' {
+                $service_start = "/usr/bin/systemctl start ${service_name}.service"
+                $service_stop = "/usr/bin/systemctl stop ${service_name}.service"
+            } else {
+                $service_start = "/sbin/service $service_name start"
+                $service_stop = "/sbin/service $service_name stop"
+            }
         }
         'Debian': {
             $package_name = 'openssh-server'
             $service_name = 'ssh'
-            $service_command = "/usr/sbin/service $service_name"
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
         'FreeBSD': {
             $service_name = 'sshd'
             $service_command = "/usr/sbin/service $service_name"
+            $service_start = "/etc/rc.d/$service_name start"
+            $service_stop = "/etc/rc.d/$service_name stop"
         }
         default: {
             $package_name = 'openssh-server'
             $service_name = 'ssh'
             $service_command = "/usr/sbin/service $service_name"
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
     }
 
